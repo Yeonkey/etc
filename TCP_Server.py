91ab72c7aafb1246d06341956bdd5c5502332c03
Yeonkey <76510434+Yeonkey@users.noreply.github.com>
2021-01-13 22:54:30 +0900

    s

---
+++ TCP_Server.py
@@ -0,0 +1,16 @@
+from socket import *
+
+serverPort = 12000
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind(('192.168.20.1',serverPort))
+serverSocket.listen(1)
+
+print("The server is ready to reveive")
+
+while True:
+    connectionSocket,addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024).decode()
+    capitalizedSentence = sentence.upper()
+    connectionSocket.send(capitalizedSentence.encode())
+    print(addr)
+    connectionSocket.close()
